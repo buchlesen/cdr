a4dab2d058364df015a2189eff5c5890bb420353
rn <associate.ru@outlook.com>
2017-04-06 18:44:51 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,16 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', 'AngularController@serveApp');
+
+// Route::get('/unsupported-browser', 'AngularController@unsupported');
+
 
 Route::get('/', 'AngularController@serveApp')->name('main.home');
 
@@ -22,4 +27,3 @@ Route::get('/', function() {
 });
 
 Route::get('/unsupported-browser', 'AngularController@unsupported');
-
